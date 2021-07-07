a804ce6ecb59545b3d607a46dd407caae8bb69dd
Gabriel Lobo <bielcraft552@gmail.com>
2021-07-06 23:41:01 -0300

    New validator handler issue #6

--- src/screens/CreateAccount/Register/index.js
+++ src/screens/CreateAccount/Register/index.js
@@ -1,59 +1,42 @@
-import * as S from './styles';
-import * as Yup from 'yup';
+import React, { useRef } from "react";
+import { Alert, Button, Image, Text, View, touco } from "react-native";
+import { Form } from "@unform/mobile";
+import { useNavigation } from "@react-navigation/native";
+import * as Yup from "yup";
+import * as Application from "expo-application";
 
-import { Alert, Button, Image, Text, View, touco } from 'react-native';
-import React, { useRef } from 'react';
-
-import { Form } from '@unform/mobile';
-import Input from './components/Input'
-import api from './../../../Services/api'
-import { useNavigation } from '@react-navigation/native';
-import * as Application from 'expo-application';
+import * as S from "./styles";
+import Input from "./components/Input";
+import api from "./../../../Services/api";
+import { Validator } from "../../../validators";
 
 // import { getUniqueId } from 'react-native-device-info';
 
-
 export default function Register(props) {
-
   const formRef = useRef(null);
   const navigation = useNavigation();
-
-
 
   async function handleSubmit(data) {
     const { name, email, password, nameofpoint, city, country, vtr } = data;
     let validationErrors = {};
+    const registerValidator = new Validator();
 
     try {
       formRef.current.setErrors({});
 
-      let schema; 
-      if (props.type == 'admin') {
-        schema =  Yup.object().shape({
-          name: Yup.string().required('Campo requerido'),
-          email: Yup.string().email().required('Campo requerido'),
-          password: Yup.string().min(6).required('Campo requerido'),
-          vtr: Yup.string().required('Campo requerido'),
-          nameofpoint: Yup.string().min(3).required('Compo requerido')
-        });
+      registerValidator.isString({ name: "name" ,value: name, msg: "Campo requerido" });
+      registerValidator.isEmail({ name: "email", value: email, msg: "Campo requerido" });
+      registerValidator.isString({ name: "password", value: password, min: 6, msg: "Campo requerido" });
+      registerValidator.isString({ name: "vtr", value: vtr, msg: "Campo requerido" });
+
+      if (props.type == "admin") {
+        registerValidator.isString({ name: "nameofpoint", value: nameofpoint, min: 3, msg: "Campo requerido" });
       }
-      else{
-        const schema = Yup.object().shape({
-          name: Yup.string().required('Campo requerido'),
-          email: Yup.string().email().required('Campo requerido'),
-          password: Yup.string().min(6).required('Campo requerido'),
-          vtr: Yup.string().required('Campo requerido'),
-        });
-      }
-      
 
+      await registerValidator.isValid();
+      /* Validation pass */
 
-      await schema.validate(data, {
-        abortEarly: false,
-      });
-      //Validation pass
-
-      const deviceId = Application.androidId
+      const deviceId = Application.androidId;
 
       const body = {
         email,
@@ -64,25 +47,20 @@ export default function Register(props) {
         vtr,
         city,
         country,
-        admin: props.type == 'admin' ? true : false
+        admin: props.type == "admin" ? true : false,
       };
-      const response = await api("POST",'/users/register',body)
-      
-      Alert.alert("Sucesso!", "Cadastro realizado com sucesso.")
+      const response = await api("POST", "/users/register", body);
 
-      navigation.navigate('Welcome')
+      Alert.alert("Sucesso!", "Cadastro realizado com sucesso.");
 
+      navigation.navigate("Welcome");
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
-        //validatoin fail
-        err.inner.forEach(error => {
-          validationErrors[error.path] = error.message;
-        });
-        formRef.current.setErrors(validationErrors);
-        console.log('Não Passou ' + err);
+        formRef.current.setErrors(registerValidator.getErrors());
+        console.log("Não Passou " + err);
       } else {
-        Alert.alert('Erro!', 'Ocorreu um erro ao se cadastrar.\n'+err)
-        console.log(err)
+        Alert.alert("Erro!", "Ocorreu um erro ao se cadastrar.\n" + err);
+        console.log(err);
       }
     }
   }
@@ -91,13 +69,9 @@ export default function Register(props) {
     <S.Container>
       <S.TopContainer>
         <S.ContainerTitle>
-          <S.Title>
-            Bem Vindo!
-          </S.Title>
+          <S.Title>Bem Vindo!</S.Title>
 
-          <S.SubTitle>
-            Cadastre-se para ter acesso ao app.
-        </S.SubTitle>
+          <S.SubTitle>Cadastre-se para ter acesso ao app.</S.SubTitle>
         </S.ContainerTitle>
         <S.ContainerInput>
           <S.ContainerIntroInput>
@@ -106,30 +80,31 @@ export default function Register(props) {
 
               <Input label="E-mail" name="email" type="email" />
               <Input label="Senha" name="password" type="password" secureTextEntry={true} />
-              <Input label="VTR" keyboardType='numeric' maxLength={3} name="vtr" type="text" />
-              {props.type === 'admin' &&
+              <Input label="VTR" keyboardType="numeric" maxLength={3} name="vtr" type="text" />
+              {props.type === "admin" && (
                 <>
                   <Input label="Nome do ponto" name="nameofpoint" />
                   <S.Space />
                   <S.ButtonsEndPage>
-                    <S.InputCoutry autoCapitalize='characters' maxLength={2} placeholder="Estado" name="state" />
+                    <S.InputCoutry
+                      autoCapitalize="characters"
+                      maxLength={2}
+                      placeholder="Estado"
+                      name="state"
+                    />
                     <S.SpaceButtons />
                     <S.InputCity placeholder="Cidade" name="city" />
                   </S.ButtonsEndPage>
                 </>
-              }
-
+              )}
 
               <S.ButtonEnter>
-                <S.TextButton onPress={() => formRef.current.submitForm()}>
-                  Cadastrar
-              </S.TextButton>
+                <S.TextButton onPress={() => formRef.current.submitForm()}>Cadastrar</S.TextButton>
               </S.ButtonEnter>
             </Form>
           </S.ContainerIntroInput>
         </S.ContainerInput>
       </S.TopContainer>
     </S.Container>
-
   );
 }
