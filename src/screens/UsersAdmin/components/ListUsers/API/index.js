86182c08a02c959aea19d84cdfaec7601191401f
NeptuneXDB <neptunex63@gmail.com>
2021-08-27 17:57:39 -0300

    Added 'handleRegisterDriverButton' at users list (Admin).

--- src/screens/UsersAdmin/components/ListUsers/API/index.js
+++ src/screens/UsersAdmin/components/ListUsers/API/index.js
@@ -1,5 +1,6 @@
 import React, { Component, useEffect } from 'react';
 import { ScrollView, Text, View } from 'react-native';
+import { useNavigation } from "@react-navigation/native";
 import List from '../../../components/ListUsers/List';
 
 import * as S from './styles'
@@ -126,14 +127,19 @@ export default class ListUsers extends Component {
   };
 
   render() {
+    const navigation = useNavigation();
 
+    function handleRegisterDriverButton() {
+      navigation.navigate("RegisterInPointAdminStack");
+    }
+    
     return (
       <S.BackPage>
         <S.TopContainer>
           <S.Container>
             <S.ListTitle>
               <S.Texto>LISTA DE USUÁRIOS</S.Texto>
-              <S.RegisterDriverButton>
+              <S.RegisterDriverButton onPress={handleRegisterDriverButton}>
                 <S.RegisterDriverButtonText>Cadastrar Motorista</S.RegisterDriverButtonText>
               </S.RegisterDriverButton>
             </S.ListTitle>
