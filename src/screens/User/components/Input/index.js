adb7592daa5ed8b7d0a4d6918543a7c9bf599b1f
NeptuneXDB <neptunex63@gmail.com>
2021-09-15 10:59:21 -0300

    Integrated API at user info screen and minor improvements.

--- src/screens/User/components/Input/index.js
+++ src/screens/User/components/Input/index.js
@@ -1,11 +1,47 @@
-import React, { useState, useEffect } from "react";
-import { Button, Image, View, Platform } from "react-native";
+import React, { useState } from "react";
+import { Alert } from "react-native";
+import * as Yup from "yup";
+
 import * as S from "./styles";
-import * as SecureStore from "expo-secure-store";
-import { useNavigation } from "@react-navigation/native";
+
+import api from "../../../../Services/api";
+import { useAuth } from "../../../../contexts/auth";
+import { Validator } from "../../../../validators";
 
 export default function Input() {
-  const navigation = useNavigation();
+  const { updateUserData, user } = useAuth();
+  const [userData, setUserData] = useState({});
+  const updateDataValidator = new Validator();
+
+  async function handleEditData(data) {
+    const { name, password, vtr } = data;
+    data = Object.fromEntries(Object.entries(data).filter(([_, v]) => !!v !== false));
+
+    if (Object.entries(data).length < 1)
+      return Alert.alert("Erro", "Nenhum dado foi modificado para ser atualizado.");
+
+    try {
+      name &&
+        updateDataValidator.isString({ name: "name", value: name, max: 300, min: 4, msg: "Campo requerido" });
+      password &&
+        updateDataValidator.isString({ name: "password", value: password, min: 8, msg: "Campo requerido" });
+      vtr && updateDataValidator.isString({ name: "vtr", value: vtr, min: 1, msg: "Campo requerido" });
+
+      await updateDataValidator.isValid();
+
+      await api("PUT", "/user/", data);
+      await updateUserData();
+      
+      Alert.alert("Sucesso", "Dados atualizados com sucesso!");
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        Alert.alert("Erro", `Não foi possível atualizar seus dados: \n${err}`);
+      } else {
+        Alert.alert("Erro", "Ocorreu um erro ao tentar atualizar seus dados.");
+      }
+    }
+  }
+
   return (
     <S.Container>
       <S.ContainerCard>
@@ -17,31 +53,21 @@ export default function Input() {
               mode="outlined"
               label="Nome"
               placeholder="Digite seu nome"
-              onChangeText={() => console.log("Pressed")}
+              onChangeText={(text) => setUserData({ ...userData, name: text })}
             />
             <S.Space />
             <S.TextInputName>CPF</S.TextInputName>
-            <S.InputName
-              editable={false}
-              mode="outlined"
-              label="CPF"
-              onChangeText={() => console.log("Pressed")}
-              />
+            <S.InputName editable={false} mode="outlined" label="CPF" />
             <S.Space />
             <S.TextInputName>E-mail</S.TextInputName>
-            <S.InputName
-              editable={false}
-              mode="outlined"
-              label="E-mail"
-              onChangeText={() => console.log("Pressed")}
-            />
+            <S.InputName editable={false} mode="outlined" label="E-mail" value={user.email} />
             <S.Space />
             <S.TextInputName>Senha</S.TextInputName>
             <S.InputVtr
               mode="outlined"
-              label="VTR"
+              label="Password"
               placeholder="Digite sua senha"
-              onChangeText={() => console.log("Pressed")}
+              onChangeText={(text) => setUserData({ ...userData, password: text })}
             />
             <S.Space />
             <S.TextInputName>VTR</S.TextInputName>
@@ -49,15 +75,10 @@ export default function Input() {
               mode="outlined"
               label="VTR"
               placeholder="Número da viatura"
-              onChangeText={() => console.log("Pressed")}
+              onChangeText={(text) => setUserData({ ...userData, vtr: text })}
             />
             <S.ContainerBtn>
-              <S.Button
-                onPress={async () => {
-                  await SecureStore.deleteItemAsync("token");
-                  navigation.navigate("Welcome");
-                }}
-              >
+              <S.Button onPress={() => handleEditData(userData)}>
                 <S.TextSave>Salvar</S.TextSave>
               </S.Button>
             </S.ContainerBtn>
