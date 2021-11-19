5b8ab684f82e1e1a36025645fcacf39862bd9801
NeptuneXDB <neptunex63@gmail.com>
2021-11-19 12:51:12 -0300

    Added redux store provider and LoadingSpinnerModalManager context provider.

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import { AuthProvider } from "./src/contexts/auth";
 import { LocationProvider } from "./src/contexts/location";
 import { WebSocketProvider } from "./src/contexts/websocket";
+import { LoadingSpinnerModalManagerProvider } from "./src/contexts/loadingSpinnerModalManager";
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
+import { Provider } from "react-redux";
 import Routes from "./src/routes";
+import store from "./src/store";
 import { StatusBar } from "expo-status-bar";
 
 export default function App() {
@@ -11,9 +14,13 @@ export default function App() {
     <NavigationContainer>
       <WebSocketProvider>
         <LocationProvider>
-          <AuthProvider>
-            <Routes />
-          </AuthProvider>
+          <Provider store={store}>
+            <LoadingSpinnerModalManagerProvider>
+              <AuthProvider>
+                <Routes />
+              </AuthProvider>
+            </LoadingSpinnerModalManagerProvider>
+          </Provider>
         </LocationProvider>
       </WebSocketProvider>
       <StatusBar style="auto" />
